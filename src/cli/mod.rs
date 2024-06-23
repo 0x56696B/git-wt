84ed6dfb254fc66d0e1d8df82c4f434c4d831571
Viktor Stamoff <transtrike@gmail.com>
2024-06-23 16:14:43 +0300

    feat: modify a bit the usage print

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -8,6 +8,14 @@ use commands::Commands;
 #[command(about = "A git extension to make `git worktree` easier to use", long_about = None)]
 #[command(version)]
 #[command(next_line_help = true)]
+#[command(help_template = "\
+{about}
+
+{usage-heading} {usage}
+
+{all-args}
+
+Author: {author-with-newline}")]
 pub struct Cli {
   #[command(subcommand)]
   pub command: Commands,
