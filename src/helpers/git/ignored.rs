99f94c71b8f83aaa0ce48d28bafb279db2cdc16a
Viktor Stamoff <transtrike@gmail.com>
2024-06-27 20:11:46 +0300

    feat: finish implementation of detecting ignored files

--- src/helpers/git/ignored.rs
+++ src/helpers/git/ignored.rs
@@ -1,12 +1,12 @@
-use git2::{Repository, Status, StatusOptions};
+use git2::{Repository, Status, StatusOptions, StatusShow};
 
 pub fn get_ignored_files(default_branch_repo: &Repository) -> Result<Vec<String>, String> {
   let mut status_opts = StatusOptions::new();
   status_opts
     .include_ignored(true)
     .recurse_ignored_dirs(true)
-    // .recurse_untracked_dirs(true)
-    .show(git2::StatusShow::IndexAndWorkdir);
+    .recurse_untracked_dirs(true)
+    .show(StatusShow::IndexAndWorkdir);
 
   let statuses =
     default_branch_repo.statuses(Some(&mut status_opts)).map_err(|e| e.message().to_string())?;
@@ -14,7 +14,9 @@ pub fn get_ignored_files(default_branch_repo: &Repository) -> Result<Vec<String>
   let ignored_paths: Vec<String> = statuses
     .iter()
     .filter(|x| x.status() == Status::IGNORED)
-    .map(|x| return x.path().unwrap().to_owned())
+    .map(|x| x.path().unwrap().to_owned())
+    // TODO: Move into function that contains all ignored patterns from config
+    .filter(|ignored| !ignored.contains("node_modules"))
     .collect();
 
   return Ok(ignored_paths);
